d39a3d7de5ee4fc951fae76585eac2e118590338
Kristian Loewe <kl@kristianloewe.com>
2016-04-25 23:01:41 +0200

    Add a convenience function for flattening an array

---
+++ flat.m
@@ -0,0 +1,11 @@
+function data = flat(data)
+%FLAT Flatten an array of data.
+%
+%   Original source:
+%     http://mri.brechmos.org/2009/04/03/flatten-an-array-of-data/
+%
+%   Author: Kristian Loewe
+
+data = data(:);
+
+end
