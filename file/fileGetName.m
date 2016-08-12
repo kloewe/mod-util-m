70204f4724f6f56d3ee08f859d5c6a1b3a5589e3
Kristian Loewe <kl@kristianloewe.com>
2016-08-12 11:53:25 +0200

    Fix indentation

--- file/fileGetName.m
+++ file/fileGetName.m
@@ -4,18 +4,18 @@ function fname = fileGetName(pathstr,varargin)
 %   fname = fileGetName(pathstr) or fname = fileGetName(pathstr,1)
 %     -> returns filename incl. extension but without folder
 %
-%   fname = fileGetFname(pathstr,0)
+%   fname = fileGetName(pathstr,0)
 %     -> returns filename without extension and without folder
 %
 %   Author: Kristian Loewe
 
 [~,fname,ext] = fileparts(pathstr);
 if nargin == 1 || (nargin == 2 && varargin{1} == 1)
-    fname = [fname,ext];
+  fname = [fname,ext];
 elseif nargin ~= 2
-    error('Wrong number of input args.');
+  error('Wrong number of input args.');
 elseif varargin{1} ~= 0
-    error('Optional input argument must be 0 or 1.');
+  error('Optional input argument must be 0 or 1.');
 end
 
 end
