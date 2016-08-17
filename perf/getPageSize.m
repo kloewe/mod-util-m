664370506d0be8693d8aee12220c3e0563062fc6
Kristian Loewe <kl@kristianloewe.com>
2016-08-17 18:00:00 +0200

    Work around some weird problems with system calls

--- perf/getPageSize.m
+++ perf/getPageSize.m
@@ -1,14 +1,29 @@
-function pagesiz = getPageSize
-%GETPAGESIZE Get page size.
+function psiz = getPageSize
+%GETPAGESIZE Get page size using getconf.
 %
-%   See also MONMEMSTART, MONMEMSTOP.
+%   S = GETPAGESIZE returns the page size in bytes.
+%
+%   See also MONMEM.
 %
 %   Author: Kristian Loewe
 
 assert(isunix, 'OS not supported.');
+[rc,msg] = system('which getconf');
+assert(~rc, 'getconf not found.\n%s', msg);
 
-[rc,pagesiz] = system('getconf PAGESIZE');
-assert(~rc, 'Could not determine page size.');
-pagesiz = str2double(pagesiz);
+% For no apparent reason, upon calling [rc,psiz] = system('getconf
+% PAGESIZE') below, psiz is occasionally empty even though rc is 0. For
+% now, I'm going to try to work around this by trying multiple times.
+% Another weird issue is that psiz occasionally contains the full path to
+% getconf, for example, /usr/bin/getconf, in addition to the page size (in
+% the majority of cases).
+psiz = []; k = 0;
+while (isempty(psiz) || isnan(psiz)) && k < 10 
+  [rc,psiz] = system('getconf PAGESIZE');
+  assert(~rc, 'Could not determine page size.');
+  psiz = str2double(psiz);
+  k = k + 1;
+end
+assert(isscalar(psiz) && ~isnan(psiz), 'Could not determine page size.');
 
 end
